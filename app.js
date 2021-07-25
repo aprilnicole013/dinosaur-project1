3851ec128dc45d1eaa68ecc61ed9ec669d3ace0b
April <aprilnicole013@gmail.com>
2021-07-25 16:45:20 -0400

    test

---
+++ app.js
@@ -0,0 +1,57 @@
+// app.js - Where you will complete the bulk of your project.
+  
+  const dino = {
+        name: "Sandy",
+        height: 60,
+        features: ["black stripes","brown skin"]
+    };
+
+    const human = {
+        name: "Patrick",
+        height: 6,
+        features: ["Muscular", "Athletic",]
+    };
+    
+
+
+
+
+// /images - Folder containing the images used for the project.
+    
+    
+    
+    
+    
+    
+    
+    // Create Dino Constructor
+
+
+    // Create Dino Objects
+
+
+    // Create Human Object
+
+    // Use IIFE to get human data from form
+
+
+    // Create Dino Compare Method 1
+    // NOTE: Weight in JSON file is in lbs, height in inches. 
+
+    
+    // Create Dino Compare Method 2
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+    
+    // Create Dino Compare Method 3
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+    // Generate Tiles for each Dino in Array
+  
+        // Add tiles to DOM
+
+    // Remove form from screen
+
+
+// On button click, prepare and display infographic
