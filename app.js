383ce0ba267cb4a87d61f55c0d72fdf54f8cda2e
April <aprilnicole013@gmail.com>
2021-07-29 23:47:27 -0400

    Refactor: Working on fetch and getting tiles on page. WIP

--- app.js
+++ app.js
@@ -12,11 +12,20 @@ function Dinosaur(species, dinoWeight, dinoHeight, dinoDiet, where, when, fact, 
 
 // Create Dino Objects
 
-//add data to array
-    // Get Dinosaur data from JSON
-fetch('dino.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+// Get Dinosaur data from JSON
+const dinojson = 'dino.json';
+
+fetch(dinojson)
+    .then(function(response) {
+    return(response.json());
+})
+    .then(function(data){
+    console.log(data)
+    
+})
+    .catch(function() {
+    console.log('An error occurred')
+});
 
 // Create Human Object
 function Human(name, weight, height, inches, diet, image) {
@@ -29,6 +38,7 @@ function Human(name, weight, height, inches, diet, image) {
 };
 
 // Use IIFE to get human data from form
+
 function getHumanData(){
     humanName = document.querySelector("#name").value;
     humanWeight = document.querySelector("#weight").value;
@@ -63,19 +73,43 @@ function compareDiet(){
     };
     
 // Generate Tiles for each Dino in Array
-function displayTiles(){
-    let tiles = document.querySelector('#grid')
-    tiles.appendChild;
-};
+function generateTiles(){
+    //create a div for each tile
+    let dinoTile = document.createElement("div")
+    dinoTile.className = "grid-item"
 
-        // Add tiles to DOM
+    // Add tiles to DOM by adding test to inner text
+    let apples = document.createElement("h2")
+    apples.innerText = "Hello, my name is"
 
-    // Remove form from screen
+    dinoTile.appendChild(apples)
 
+    document.getElementById("grid").appendChild(dinoTile)
+
+    let orangesDiv = document.createElement("div")
+    orangesDiv.className = "grid-item"
+    
+    let oranges = document.createElement("h2")
+    oranges.innerText = "Oranges"
+
+    orangesDiv.appendChild(oranges)
+    //append new elelment to new div
+
+    //create element on page
+    document.getElementById("grid").appendChild(orangesDiv)
+
+ };
+
+// Remove form from screen
+function removehumanForm(){
+    document.querySelector("#dino-compare").style.display = "none"
+}
 
 // On button click, prepare and display infographic
 const compareBtn = document.querySelector('#btn');
 
 compareBtn.addEventListener("click", function(){
-    displayTiles()
+        removehumanForm();
+        generateTiles();
+        console.log(getHumanData());
 });
